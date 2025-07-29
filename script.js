f12086b3d81876a2df972c85ebde09646b937065
Bakir Bhuyain <hemalbakir@gmail.com>
2025-07-29 18:34:12 +0600

    Update script.js

--- script.js
+++ script.js
@@ -43,5 +43,35 @@
 // console.log(ul.nextSibling);
 
 //manupulate the dom
-const ul = document.querySelector('ul');
-ul.innerHTML = '<p>hello</p>';
+// const ul = document.querySelector('ul');
+// ul.innerHTML = '<p>hello</p>';
+
+// document.querySelector('h2').removeAttribute('class')
+// document.querySelector('h2').setAttribute('class','first-heading');
+//  const att = document.querySelector('h2').getAttribute('id');
+//  console.log(att);
+
+
+// const ul = document.querySelector('ul');
+// const li = document.createElement('li');
+// li.innerText = 'added by js';
+// ul.appendChild(li);
+
+// const ul1 = document.querySelector('h2');
+// const h2 = document.createElement('h2');
+// h2.innerText = 'added by js';
+// ul1.appendChild(h2);
+
+// add class name || classList[odd(),remove()]
+
+// const h2 = document.querySelector('h2');
+// console.log(h2);
+// h2.classList.add('first','second');
+// h2.classList.remove('second');
+
+const ul = document.querySelector('ul');//age amra ul take dhorchi
+const second = document.getElementById('second');//j jinistate amra notun element take dite chai oitake id diye dorchi
+
+const li = document.createElement('li');//li create korchi
+li.innerText = 'hello';//vhitorer textt ta likhchi
+ul.insertBefore(li , second);//vhitore diyechi
